906906c12d85d9c0469d7a29da2ac7a92b012e2e
bunmigrey <bunmigrey@icloud.com>
2023-06-19 06:46:10 +0100

    confirm verify auth function??

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Headers, Put, Req, Res } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpStatus, Redirect, Param, Post, Headers, Put, Req, Res } from "@nestjs/common";
 import { User } from "./schema/user.schema";
 import { UserService } from "./user.service";
 import { UserDto } from "./dto/user.dto"
 // import { JwtService } from '@nestjs/jwt'
 
-@Controller('user')
+@Controller()
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
-    @Post('signup')
+    @Get()
+    @Redirect('https://documenter.getpostman.com/view/26141564/2s93si2AR7', 301)
+    redirectToWebsite() {}
+
+
+    @Post('user/signup')
     async Signup(@Res() response, @Body() user: User) {
         const newUSer = await this.userService.signup(user);
         return response.status(HttpStatus.CREATED).json({
@@ -16,13 +21,13 @@ export class UserController {
         })
     }
 
-    @Post('signin')
+    @Post('user/signin')
     async SignIn(@Res() response, @Body() user: UserDto) {
         const token = await this.userService.signin(user);
         return response.status(HttpStatus.OK).json(token)
     }
 
-    @Get('verify')
+    @Get('user/verify')
     async verifyAuth (@Headers('authorization') authHeader: string, @Req() request: object){
         return this.userService.verifyAuth(authHeader, request)
     }
